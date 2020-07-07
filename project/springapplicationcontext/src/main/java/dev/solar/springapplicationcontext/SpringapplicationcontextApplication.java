f5b79733066872842b6573a869d2ef3d61d81f03
blossun <dnsngu@gmail.com>
2020-07-07 21:36:38 +0900

    [Feat] : xml에 bean 태그로 직접 작성하여 bean을 생성
    고전적인 방식으로 이렇게 거의 쓸 일이 없음
    하나하나 빈으로 직접 등록하고 의존성 주입을 해줘야 한다.

--- project/springapplicationcontext/src/main/java/dev/solar/springapplicationcontext/SpringapplicationcontextApplication.java
+++ project/springapplicationcontext/src/main/java/dev/solar/springapplicationcontext/SpringapplicationcontextApplication.java
@@ -1,13 +1,20 @@
 package dev.solar.springapplicationcontext;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import java.util.Arrays;
+
 public class SpringapplicationcontextApplication {
+    private static final Logger log = LoggerFactory.getLogger(SpringapplicationcontextApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringapplicationcontextApplication.class, args);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        log.debug("생성된 빈의 이름 : {}", Arrays.toString(beanDefinitionNames));
+        BookService bookService = (BookService) context.getBean("bookService");//Type cast
+        log.debug("의존성 주입이 되었는지 확인 : {}",bookService.bookRepository != null);//null이 아닌지 확인 -> true : 빈주입 성공
     }
 
 }
