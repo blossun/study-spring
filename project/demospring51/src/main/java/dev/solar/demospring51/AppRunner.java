afb3a4d5d4dbf12adfa7db94703609437f740c98
blossun <dnsngu@gmail.com>
2020-07-08 20:26:54 +0900

    [Feat] : 빈의 스코프 - 프로토타입
    프로토타입은 호출 시마다 새로운 인스턴스를 만들고, 싱글톤은 동일한 인스턴스임을 확인

--- project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
@@ -3,22 +3,29 @@ package dev.solar.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    Single single;
 
     @Autowired
-    Proto proto;
-
+    ApplicationContext ctx;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(proto); //AppRunner가 주입받은 proto
-        System.out.println(single.getProto()); //Single 객체가 주입받은 proto
+        System.out.println("proto");
+
+        System.out.println(ctx.getBean(Proto.class));
+        System.out.println(ctx.getBean(Proto.class));
+        System.out.println(ctx.getBean(Proto.class));
+
+        System.out.println("single");
+
+        System.out.println(ctx.getBean(Single.class));
+        System.out.println(ctx.getBean(Single.class));
+        System.out.println(ctx.getBean(Single.class));
     }
 }
 
