06726e7b108995ac1ec9ed81f8ffdfe412fd48f7
blossun <dnsngu@gmail.com>
2020-07-08 17:01:41 +0900

    [Feat] : [@Component와 컴포넌트 스캔] - 펑션을 사용한 빈 등록
    인스턴스를 만들어서 스프링 부트를 구동하는 방식
    GenericApplicationContext의 registerBean()으로 원하는 ApplicationContext를 직접 주입할 수 있다.

--- project/demospring51/src/main/java/dev/solar/demospring51/Demospring51Application.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/Demospring51Application.java
@@ -1,13 +1,31 @@
 package dev.solar.demospring51;
 
+import dev.solar.out.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Demospring51Application {
 
+    @Autowired
+    MyService myService;
+
     public static void main(String[] args) {
-        SpringApplication.run(Demospring51Application.class, args);
+        SpringApplication app = new SpringApplication(Demospring51Application.class);
+        app.addInitializers(new ApplicationContextInitializer<GenericApplicationContext>() {
+            @Override
+            public void initialize(GenericApplicationContext ctx) {
+                ctx.registerBean(MyService.class);
+            }
+        });
+        app.run(args);
     }
 
 }
---
+++ project/demospring51/src/main/java/dev/solar/out/MyService.java
@@ -0,0 +1,4 @@
+package dev.solar.out;
+
+public class MyService {
+}
