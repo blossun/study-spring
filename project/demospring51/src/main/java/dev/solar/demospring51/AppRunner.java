2896380b118b134e1059834fe0d5ffee4523adff
blossun <dnsngu@gmail.com>
2020-07-09 14:34:46 +0900

    [Feat] : [ Environment 2부. 프로퍼티] VM options로 프로퍼티 설정
    자바 애플리케이션을 실행할 때 넘겨주는 프로퍼티 설정
    * VM options : -Dapp.name=spring5

--- project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
@@ -7,22 +7,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
     ApplicationContext ctx;
 
-    @Autowired
-    BookRepository bookRepository;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name"));
     }
 }
 
