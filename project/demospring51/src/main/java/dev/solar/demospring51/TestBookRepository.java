a4d10200ae8039cf1723b6e35f7c345de0101d60
blossun <dnsngu@gmail.com>
2020-07-09 14:15:09 +0900

    [Feat] : 클래스에 프로파일 정의 - (2) Component
    컴포넌트 스캔으로 등록되는 빈에도 프로파일을 정의할 수 있다.

--- project/demospring51/src/main/java/dev/solar/demospring51/TestBookRepository.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/TestBookRepository.java
@@ -1,4 +1,9 @@
 package dev.solar.demospring51;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("!prod & test")
 public class TestBookRepository implements BookRepository{
 }
