13db8df857dcdbd0a449ec3800f2eb7c9a22dfa6
blossun <dnsngu@gmail.com>
2020-07-10 11:40:25 +0900

    [Feat] : 다수개 핸들러가 존재할 때 이벤트 처리 (3) - 비동기적으로 처리
    * 핸들러에 @Async
    * main 클래스에 @EnableAsync

--- project/demospring51/src/main/java/dev/solar/demospring51/AnotherHandler.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AnotherHandler.java
@@ -1,17 +1,16 @@
 package dev.solar.demospring51;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
 
-     @EventListener
-     @Order(Ordered.HIGHEST_PRECEDENCE + 2)
+    @EventListener
+    @Async
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
-        System.out.println("Another Handler !!! 데이터는 : " + myEvent.getData() );
+        System.out.println("Another Handler !!! 데이터는 : " + myEvent.getData());
     }
 }
--- project/demospring51/src/main/java/dev/solar/demospring51/Demospring51Application.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/Demospring51Application.java
@@ -2,8 +2,10 @@ package dev.solar.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Demospring51Application {
 
     public static void main(String[] args) {
