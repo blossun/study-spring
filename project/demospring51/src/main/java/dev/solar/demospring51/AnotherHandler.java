10c1f81d5cb6f80538f49e4c79503776c1fdc7dc
blossun <dnsngu@gmail.com>
2020-07-10 11:28:56 +0900

    [Feat] : 다수개 핸들러가 존재할 때 이벤트 처리 (2) - 순서를 정해서 처리
    EventHandler에 `@Order`어노테이션으로 우선순위를 부여하면 된다.

--- project/demospring51/src/main/java/dev/solar/demospring51/AnotherHandler.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AnotherHandler.java
@@ -1,12 +1,15 @@
 package dev.solar.demospring51;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
 
-    @EventListener
+     @EventListener
+     @Order(Ordered.HIGHEST_PRECEDENCE + 2)
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("Another Handler !!! 데이터는 : " + myEvent.getData() );
--- project/demospring51/src/main/java/dev/solar/demospring51/MyEventHandler.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/MyEventHandler.java
@@ -1,12 +1,14 @@
 package dev.solar.demospring51;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
-
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 전달 받음!!!! 데이터는 : " + event.getData());
