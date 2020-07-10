631e601990039f75bd6636950ec87558a651cb3d
blossun <dnsngu@gmail.com>
2020-07-10 16:27:13 +0900

    [Feat] : [Resource 추상화]
    java.net.URL을 추상화하여 `org.springframework.core.io.Resource` 라는 클래스로 감싸서 실제 로우 레벨에 있는 리소스에 접근하는 기능을 만든 것이다.

--- project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
@@ -3,20 +3,29 @@ package dev.solar.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass()); //WebApplicationContext 중 하나
+
         Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass()); //classpath prefix를 썼기때문에 ClassPathResource 타입
+
+        Resource resource02 = resourceLoader.getResource("text.txt");
+        System.out.println(resource02.getClass()); //기본 타입 ServletContextResource
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription()); //전체 경로
+        System.out.println(resource02.exists());
+        System.out.println(resource02.getDescription()); //전체 경로
     }
 }
