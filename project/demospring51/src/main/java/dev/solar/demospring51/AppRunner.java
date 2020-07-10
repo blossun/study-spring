8645c9ba13e24f61e31df0df576acb9b3ec70ae1
blossun <dnsngu@gmail.com>
2020-07-10 14:32:20 +0900

    [Proj] : [ResourceLoader] 생성
    리소스를 읽어오는 기능
    클래스패쓰에서 읽어와서 파일이 존재하는지 여부를 출력

--- project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
+++ project/demospring51/src/main/java/dev/solar/demospring51/AppRunner.java
@@ -3,18 +3,20 @@ package dev.solar.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher publisherEvent;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // 이벤트 발생
-        publisherEvent.publishEvent(new MyEvent(this, 100));
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription()); //전체 경로
     }
 }
