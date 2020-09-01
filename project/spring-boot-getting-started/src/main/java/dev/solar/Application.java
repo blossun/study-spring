b870a2616d22f1ec914f05e3c29d5fcbc39237dd
blossun <dnsngu@gmail.com>
2020-09-01 14:36:56 +0900

    [Proj] @Configuration, @ComponentScan 어노테이션으로 어플리케이션 실행
    웹 서버로 실행시키지 않고, 일반 어플리테이션으로 실행

--- project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
+++ project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
@@ -1,12 +1,17 @@
 package dev.solar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE); //WebApplicationType 으로 실행하지 않음
+        application.run(args);
     }
 }
