0bd3da5a61f1db8e23732267f2df46574a71765f
blossun <dnsngu@gmail.com>
2020-09-01 17:29:52 +0900

    [자동 설정 만들기 1부] 다른 외부 프로젝트에서 방금 만든 의존성을 추가
    [solar-spring-boot-starter] 프로젝트에서 만들어 놓은 Holoman 자동 설정 파일을 로컬 메이브 저장소에 설치하여,
    [spring-boot-getting-started] 프로젝트에서 의존성을 추가하여 해당 설정 파일을 가져와 사용
    
    [spring-boot-getting-started] 프로젝트에서 Holoman에 대한 빈을 별도로 등록하지 않았지만, pom.xml에 [solar-spring-boot-starter] 프로젝트에 대한 의존성을 추가하였기 때문에 자동으로 Holoman 빈이 생성되어 사용할 수 있다.

--- project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
+++ project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
@@ -1,6 +1,7 @@
 package dev.solar;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE); //좀 더 빨리 실행되도록 타입 변경
         application.run(args);
     }
 }
