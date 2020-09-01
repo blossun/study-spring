c417347552e25bb8c783ac5932887ad43ce13375
blossun <dnsngu@gmail.com>
2020-09-01 15:10:57 +0900

    [Proj] @SpringBootApplication 을 이용한 어플리케이션 실행
    "모든 jar 파일에서 `spring.factories`  파일을 찾아서 키값(ex, `EnableAutoConfiguration` )해당하는 모든 클래스들을 보고 컨디션에 맞으면 빈을 등록한다."
    `@SpringBootApplication` 어노테이션을 통해서 `spring.factories`  에 정의된 수많은 자동 설정들이 조건에 따라 적용이 되어서 수많은 빈들이 생성이 되고, 내장 톰캣을 사용해서 Web 어플리케이션이 구동되는 것이다.

--- project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
+++ project/spring-boot-getting-started/src/main/java/dev/solar/Application.java
@@ -1,17 +1,13 @@
 package dev.solar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE); //WebApplicationType 으로 실행하지 않음
         application.run(args);
     }
 }
