6fd68f19c8f2ac5e6eba2a565a2bcab5d407567e
blossun <dnsngu@gmail.com>
2020-09-05 20:12:00 +0900

    [외부 설정 2부] - 프로퍼티 값 검증
    올바른 값이 들어왔는지 확인할 수 있는 Validation API 사용 가능

--- project/spring-application-1/src/main/java/dev/solar/springinit/PersonProperties.java
+++ project/spring-application-1/src/main/java/dev/solar/springinit/PersonProperties.java
@@ -3,16 +3,22 @@ package dev.solar.springinit;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 @Component
 @ConfigurationProperties("person")
+@Validated
 public class PersonProperties {
 
+    @NotEmpty
     private String name;
 
+//    @Size(min = 20, max = 100) //바인딩 에러남... 왜지?
     private int age;
 
     private String fullName;
