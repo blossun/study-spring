208f199400d65c034a6d8c3f9f95ffee0da12bad
blossun <dnsngu@gmail.com>
2020-09-08 17:39:23 +0900

    [Proj] - [테스트 유틸] OutputCaptureRule
    * ~~OutputCapture~~는 2.2.0 부터 Deprecated 됨
    * OutputCaptureRule 객체 public하게 생성

--- project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
+++ project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
@@ -1,13 +1,16 @@
 package dev.solar.springtestdemo.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,6 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class) //이 컨트롤러 하나만 테스트
 public class SampleControllerTest {
+
+    @Rule
+    public OutputCaptureRule output = new OutputCaptureRule();
 
     @Autowired
     MockMvc mockMvc; //@WebMvcTest는 `MockMvc로 테스트해야한다.
@@ -26,10 +32,15 @@ public class SampleControllerTest {
 
     @Test
     public void hello() throws Exception {
+
         //mocking
         when(mockSampleService.getName()).thenReturn("Solar");
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello Solar"));
+
+        assertThat(output)
+                .contains("nunnu")
+                .contains("skip");
     }
 }
