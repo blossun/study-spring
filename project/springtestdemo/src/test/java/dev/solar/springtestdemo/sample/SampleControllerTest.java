0a7296b46820179dacd76f8f31ce0cddcb4794fb
blossun <dnsngu@gmail.com>
2020-09-08 13:02:46 +0900

    [Proj] - [테스트] WebEnvironment - Mock 환경
    * mock servlet environment. 내장 톰캣 구동 안 함.
    1. MockMvc 생성
    2. MockMvc로 hello controller를 테스트
    3. `print`로 찍히는 대부분의 정보를 테스트해볼 수 있다.

--- project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
+++ project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
@@ -1,11 +1,31 @@
 package dev.solar.springtestdemo.sample;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class SampleControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello")) // <-- /hello url에 get 요청에 대한 테스트
+                .andExpect(status().isOk()) // <-- response status 값 200 체크
+                .andExpect(content().string("hello Solar")) // <-- response body 값 string으로 체크
+                .andDo(print()); // <-- 요청 정보 출력
+    }
 }
