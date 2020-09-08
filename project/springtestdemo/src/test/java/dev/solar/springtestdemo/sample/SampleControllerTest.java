f9175214b0b7536c97d21485d88f1bacba59383c
blossun <dnsngu@gmail.com>
2020-09-08 13:45:02 +0900

    [Proj] - [테스트] @MockBean
    - `@MockBean` 어노테이션으로 테스트하려는 컨트롤러와 연관관계있는 서비스를 mockSampleService로 만들면, ApplicationContext에 들어있는 빈을 Mock으로 만든 객체로 교체 한다.
    - 모든 @Test 마다 자동으로 리셋.
    - Service 단까지 들어가지 않고 controller만 테스트할 수 있음

--- project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
+++ project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,9 +18,17 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate; //테스트용 TestTemplate
 
+    //controller와 연관있는 service를 MockBean으로 만듬
+    //ApplicationContext에 들어있는 빈을 Mock으로 만든 객체로 교체
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        //mocking
+        when(mockSampleService.getName()).thenReturn("Holari"); //테스트를 위해 실제 SampleService.getName()이 리턴하는 값과 다르게 주었음
+
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello Solar");
+        assertThat(result).isEqualTo("hello Holari");
     }
 }
