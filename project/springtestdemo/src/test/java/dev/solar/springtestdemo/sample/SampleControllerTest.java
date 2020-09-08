9f498fe40ef65d5f17fa46cd2ba5dcdfba5ec629
blossun <dnsngu@gmail.com>
2020-09-08 14:32:23 +0900

    [Proj] - [테스트] WebTestClient 사용
    `WebClient`는 `asynchronous(비동기)`이다. 요청을 보내고 기다리는 것이 아니라 응답이 오면 그 때 우리한테 콜백(이벤트)이 온다. 그러면 그 때 콜백을 실행할 수 있다.
    그런식으로 비동기적으로 동작하기때문에 우리가 테스트코드에서도 `WebTestclient`를 이용해서 `WebClient`와 비슷한 API를 사용할 수 있다.
    * spring-boot-starter-webflux 의존성 추가 필요

--- project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
+++ project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
@@ -5,10 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -16,7 +15,7 @@ import static org.mockito.Mockito.when;
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate; //테스트용 TestTemplate
+    WebTestClient webTestClient; //WebClient와 비슷하게 동작하는 비동기 처리방식의 API
 
     //controller와 연관있는 service를 MockBean으로 만듬
     //ApplicationContext에 들어있는 빈을 Mock으로 만든 객체로 교체
@@ -26,9 +25,10 @@ public class SampleControllerTest {
     @Test
     public void hello() throws Exception {
         //mocking
-        when(mockSampleService.getName()).thenReturn("Holari"); //테스트를 위해 실제 SampleService.getName()이 리턴하는 값과 다르게 주었음
+        when(mockSampleService.getName()).thenReturn("Solar");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello Holari");
+        webTestClient.get().uri("/hello")
+                .exchange().expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello Solar");
     }
 }
