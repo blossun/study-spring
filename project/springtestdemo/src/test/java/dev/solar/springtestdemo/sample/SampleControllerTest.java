912c5a2e600060a4569a83b01f678fd44bc1a35c
blossun <dnsngu@gmail.com>
2020-09-08 16:13:54 +0900

    [Proj] - [테스트] 슬라이스 테스트
    레이어별로 잘라서 필요한 빈만 주입받아서 테스트

--- project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
+++ project/springtestdemo/src/test/java/dev/solar/springtestdemo/sample/SampleControllerTest.java
@@ -3,19 +3,21 @@ package dev.solar.springtestdemo.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class) //이 컨트롤러 하나만 테스트
 public class SampleControllerTest {
 
     @Autowired
-    WebTestClient webTestClient; //WebClient와 비슷하게 동작하는 비동기 처리방식의 API
+    MockMvc mockMvc; //@WebMvcTest는 `MockMvc로 테스트해야한다.
 
     //controller와 연관있는 service를 MockBean으로 만듬
     //ApplicationContext에 들어있는 빈을 Mock으로 만든 객체로 교체
@@ -27,8 +29,7 @@ public class SampleControllerTest {
         //mocking
         when(mockSampleService.getName()).thenReturn("Solar");
 
-        webTestClient.get().uri("/hello")
-                .exchange().expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello Solar");
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("hello Solar"));
     }
 }
