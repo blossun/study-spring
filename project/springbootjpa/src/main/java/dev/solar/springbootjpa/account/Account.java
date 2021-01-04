a8e4f2d526a8fbb12771c5a8ad0ee019baa2475d
blossun <dnsngu@gmail.com>
2021-01-04 16:48:12 +0900

    [스프링 데이터] 데이터베이스 마이그레이션 - Flyway
    스키마와 데이터를 버전관리할 수 있다.
    
    issue #7

--- project/springbootjpa/src/main/java/dev/solar/springbootjpa/account/Account.java
+++ project/springbootjpa/src/main/java/dev/solar/springbootjpa/account/Account.java
@@ -16,6 +16,8 @@ public class Account {
 
     private String password;
 
+    private boolean active;
+
     public Long getId() {
         return id;
     }
@@ -38,6 +40,14 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override
