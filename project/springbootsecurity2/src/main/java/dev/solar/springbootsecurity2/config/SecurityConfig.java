0defe42bb7daf02e313c1b5b1680b1566e26f9b0
blossun <dnsngu@gmail.com>
2021-01-06 02:04:19 +0900

    [스프링 시큐리티] 3. PasswordEncoder 설정 및 사용
    1. 패스워드 인코더 설정
    스프링 시큐리티가 권장하는 패스워드 인코더를 사용하도록 빈으로 등록
    2. 유저 정보를 저장하기 전에도 이 인코더를 사용해서 패스워드를 인코딩한 값으로 저장하도록 해야함
    => 로그인 성공
    
    issue #9

--- project/springbootsecurity2/src/main/java/dev/solar/springbootsecurity2/config/SecurityConfig.java
+++ project/springbootsecurity2/src/main/java/dev/solar/springbootsecurity2/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package dev.solar.springbootsecurity2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,5 +19,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin() // 폼 로그인 사용
                 .and()
                 .httpBasic(); // Basic 인증도 사용
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
