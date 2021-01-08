f0b1f81f58f87a3f614f5f1a8d9605a548d4cbd9
blossun <dnsngu@gmail.com>
2021-01-08 12:24:30 +0900

    test: Event 생성 API 테스트 코드
    - Response로 201 상태코드, JSON 타입의 응답이 오는지
    
    issue #15

---
+++ project/rest-api-with-spring/src/test/java/dev/solar/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,29 @@
+package dev.solar.demoinflearnrestapi.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/") // 요청
+        .contentType(MediaType.APPLICATION_JSON) // Request body 데이터 형태
+        .accept(MediaTypes.HAL_JSON)) // Response 데이터 타입
+                .andExpect(status().isCreated()); // 201 응답코드
+    }
+}
