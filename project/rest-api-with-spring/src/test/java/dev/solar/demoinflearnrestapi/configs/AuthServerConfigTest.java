845f1f0f28ce13bf9713ec843a07b247368354a7
blossun <dnsngu@gmail.com>
2021-01-20 23:35:40 +0900

    feat: 스프링 시큐리티 OAuth2 인증 서버 설정
    토큰 발생 테스트 통과
    - Response Body에서 발행된 토큰정보 확인
    
    issue #19

--- project/rest-api-with-spring/src/test/java/dev/solar/demoinflearnrestapi/configs/AuthServerConfigTest.java
+++ project/rest-api-with-spring/src/test/java/dev/solar/demoinflearnrestapi/configs/AuthServerConfigTest.java
@@ -7,11 +7,9 @@ import dev.solar.demoinflearnrestapi.common.BaseControllerTest;
 import dev.solar.demoinflearnrestapi.common.TestDescription;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.header.Header;
 
 import java.util.Set;
 
-import static org.junit.Assert.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,21 +25,21 @@ public class AuthServerConfigTest extends BaseControllerTest {
     @TestDescription("인증 토큰을 발급 받는 테스트")
     public void getAuthToken() throws Exception {
         // Given
-        String username = "solar@email.com";
-        String password = "solar";
-        Account solar = Account.builder()
+        String username = "tester@email.com";
+        String password = "1234";
+        Account keesun = Account.builder()
                 .email(username)
                 .password(password)
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
-        this.accountService.saveAccount(solar);
+        this.accountService.saveAccount(keesun);
 
         String clientId = "myApp";
         String clientSecret = "pass";
 
         // When & Then
         this.mockMvc.perform(post("/oauth/token")
-                    .with(httpBasic(clientId, clientSecret)) // Basic OAuth Header
+                    .with(httpBasic(clientId, clientSecret))
                     .param("username", username)
                     .param("password", password)
                     .param("grant_type", "password"))
